bd623231b9460e81bcaaceebbfa6c69c75d9f045
lmohade3l <mohade3.2710@gmail.com>
2023-07-25 21:07:41 +0330

    ajax req+view tweet+error

--- tweets/models.py
+++ tweets/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import random
 
 # Create your models here.
 class Tweet(models.Model) :
@@ -8,3 +9,14 @@ class Tweet(models.Model) :
     content = models.TextField(blank=True , null=True)
     #a path to the image will be saved in the database. 
     image = models.FileField(upload_to='images/' , blank=True, null=True)  
+
+
+    class Meta:
+        ordering = ['-id']
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'content': self.content,
+            'likes': random.randint(0,50)
+        }
--- tweets/views.py
+++ tweets/views.py
@@ -14,20 +14,33 @@ def home_view(request , *args , **kwargs) :
     return render(request , 'pages/home.html' , context={} , status=200)
 
 def tweet_create_view(request , *args , **kwargs ):
+    print(request.is_ajax)
     form = TweetForm(request.POST or None)
     next_url = request.POST.get('next') or None
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
+
+        if request.is_ajax:
+            return JsonResponse(obj.serialize() , status=201)
+            
         if next_url != None and is_safe_url(next_url , ALLOWED_HOSTS):
             return redirect(next_url)
+        
         form = TweetForm()
+
+    if form.errors:
+        if request.is_ajax:
+            return JsonResponse(form.errors , status=400)
+        
     return render(request , 'components/form.html' , context={'form':form})
 
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
-    tweets_list = [{'id':x.id , 'content':x.content , 'likes':random.randint(0,100)} for x in qs]
+    # tweets_list = [{'id':x.id , 'content':x.content , 'likes':random.randint(0,100)} for x in qs]
+    tweets_list = [x.serialize() for x in qs]
     data = {
+        'is_user' : False,
         'response': tweets_list
     }
     return JsonResponse(data)
